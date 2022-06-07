10ee3e648e82526eaa519546bc6832724602ae75
Ted Glynn <will.glynn@gmail.com>
2022-06-06 22:21:45 -0600

    set up initial routes to landing page and notes page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const notes = require("./Develop/db/db.json");
+const notes = require("./db/db.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3001;
@@ -8,16 +9,25 @@ const PORT = 3001;
 //MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
-
 app.use(express.static("public"));
 
+// GET route for landing page
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-app.get("/api/notes", (req, res) => {
+// GET route for note-taking page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/notes", (req, res) => {
   res.status(200).json(notes);
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(`${req.method} request received to get notes`);
+  console.info(`${req.method} request received to get notes`);
 });
 
 app.listen(PORT, () =>
