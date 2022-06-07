a83eb2d3d0564da5119a5dfe578dd731a8272f9d
Ted Glynn <will.glynn@gmail.com>
2022-06-07 16:24:09 -0600

    resolved port issue

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const uuid = require("./middleware/uuid.js");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //MIDDLEWARE
 app.use(express.json());
